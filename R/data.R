73fe11b1103bf28825f9fd829c3f54c5cd9a0357
RWParsons <rex.parsons94@gmail.com>
2023-10-04 18:45:58 +1100

    initial commit with some spatial data

---
+++ R/data.R
@@ -0,0 +1,61 @@
+#' \code{sf} Multipolygon data for the Primary Health Network boundaries for Australia.
+#'
+#'
+#' @format ## `phn`
+#'
+#' A \code{sf} object.
+#'
+#'
+#' shows the boundaries of the Primary Health Network in Australia.
+#' @source <https://data.gov.au/dataset/ds-dga-ef2d28a4-1ed5-47d0-8e3a-46e25bc4f66b/details>
+"phn"
+
+
+#' \code{sf} Multipolygon data for the Local Hospital Network boundaries for Australia.
+#'
+#'
+#' @format ## `lhn`
+#'
+#' A \code{sf} object.
+#'
+#'
+#' shows the boundaries of the Local Hospital Network in Australia.
+#' @source <https://hub.arcgis.com/datasets/ACSQHC::local-hospital-networks/explore>
+#' and <https://discover.data.vic.gov.au/dataset/dhhs-service-areas> for Victoria.
+"lhn"
+
+
+#' \code{sf} Coordinates data (centroids) for the meshblocks (2021 edition) for Australia including population counts (\code{Person}).
+#'
+#'
+#' @format ## `mb21_pop`
+#'
+#' A \code{sf} object.
+#'
+#'
+#' @source Mesh Blocks 2021 shapefile <https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files> and population data <https://www.abs.gov.au/census/guide-census-data/mesh-block-counts/latest-release>.
+"mb21_pop"
+
+
+#' \code{sf} Polygons data for the meshblocks (2021 edition) for Australia including population counts (\code{Person}). (NSW and VIC only.)
+#'
+#'
+#' @format ## `mb21_poly_A`
+#'
+#' A \code{sf} object.
+#'
+#'
+#' @source Mesh Blocks 2021 shapefile <https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files> and population data <https://www.abs.gov.au/census/guide-census-data/mesh-block-counts/latest-release>.
+"mb21_poly_A"
+
+
+#' \code{sf} Polygons data for the meshblocks (2021 edition) for Australia including population counts (\code{Person}). (States othe than NSW and VIC only.)
+#'
+#'
+#' @format ## `mb21_poly_B`
+#'
+#' A \code{sf} object.
+#'
+#'
+#' @source Mesh Blocks 2021 shapefile <https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files> and population data <https://www.abs.gov.au/census/guide-census-data/mesh-block-counts/latest-release>.
+"mb21_poly_B"
