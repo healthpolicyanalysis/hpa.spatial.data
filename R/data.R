4cd8354b1a923d6bff128d00dad4a131d3761aac
RWParsons <rex.parsons94@gmail.com>
2023-10-12 16:22:56 +1100

    add mmm 2019 data (actually just a data.frame with SA1_16 and mmm_19)

--- R/data.R
+++ R/data.R
@@ -59,3 +59,16 @@
 #'
 #' @source Mesh Blocks 2021 shapefile <https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files> and population data <https://www.abs.gov.au/census/guide-census-data/mesh-block-counts/latest-release>.
 "mb21_poly_B"
+
+
+#' A \code{data.frame} for the SA1 (2016 edition) and associated Modified Monash Model (MMM) classification (2019 edition) for Australia including population counts (\code{Person}). (States othe than NSW and VIC only.)
+#'
+#'
+#' @format ## `mmm19`
+#'
+#' A \code{data.frame}.
+#'
+#'
+#' @source Mesh Blocks 2021 shapefile <https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files> and population data <https://www.abs.gov.au/census/guide-census-data/mesh-block-counts/latest-release>.
+"mmm19"
+
